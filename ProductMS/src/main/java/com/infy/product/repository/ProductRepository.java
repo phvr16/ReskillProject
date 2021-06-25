4905701a07f4b6a571dd3e09753365f9fe605680
Ponugoti Harsha <harsha.ponugoti11@gmail.com>
2021-06-25 14:13:00 +0530

    Added APIs in controller and methods in repository

--- ProductMS/src/main/java/com/infy/product/repository/ProductRepository.java
+++ ProductMS/src/main/java/com/infy/product/repository/ProductRepository.java
@@ -1,5 +1,15 @@
 package com.infy.product.repository;
 
-public class ProductRepository {
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.infy.product.entity.Product;
 
+public interface ProductRepository extends CrudRepository<Product, String>{
+    public Product findByProdId(String id);
+	
+	public Product findByProductName(String name);
+	
+	public List<Product> findByCategory(String category);
+	
+	public List<Product> findAll();
 }
--- ProductMS/src/main/java/com/infy/product/service/ProductService.java
+++ ProductMS/src/main/java/com/infy/product/service/ProductService.java
@@ -1,5 +1,21 @@
 package com.infy.product.service;
 
-public class ProductService {
+import java.util.List;
 
+import com.infy.product.dto.ProductDTO;
+
+public interface ProductService {
+public String addProduct(ProductDTO productDTO) ;
+	
+	public String deleteProduct(String id) ;
+	
+	public ProductDTO getProductByName(String name);
+	
+	public List<ProductDTO> getProductByCategory(String category);
+	
+	public ProductDTO getProductById(String id);
+
+	public Boolean updateStockOfProd(String prodId, Integer quantity);
+	
+	public List<ProductDTO> viewAllProducts();
 }
