2a41be0e725869731e7fe947b39753a575fb9c56
Ponugoti Harsha <harsha.ponugoti11@gmail.com>
2021-06-25 16:14:57 +0530

    Created exceptionclass,Added DTO and serviceimpl classes

---
+++ ProductMS/src/main/java/com/infy/product/exception/ProductMSException.java
@@ -0,0 +1,7 @@
+package com.infy.product.exception;
+
+public class ProductMSException extends Exception{
+	public ProductMSException(String message) {
+		super(message);
+	}
+}
--- ProductMS/src/main/java/com/infy/product/service/ProductService.java
+++ ProductMS/src/main/java/com/infy/product/service/ProductService.java
@@ -3,19 +3,20 @@ package com.infy.product.service;
 import java.util.List;
 
 import com.infy.product.dto.ProductDTO;
+import com.infy.product.exception.ProductMSException;
 
 public interface ProductService {
-public String addProduct(ProductDTO productDTO) ;
+public String addProduct(ProductDTO productDTO) throws ProductMSException ;
 	
-	public String deleteProduct(String id) ;
+	public String deleteProduct(String id) throws ProductMSException ;
 	
-	public ProductDTO getProductByName(String name);
+	public ProductDTO getProductByName(String name) throws ProductMSException;
 	
-	public List<ProductDTO> getProductByCategory(String category);
+	public List<ProductDTO> getProductByCategory(String category) throws ProductMSException;
 	
-	public ProductDTO getProductById(String id);
+	public ProductDTO getProductById(String id) throws ProductMSException;
 
-	public Boolean updateStockOfProd(String prodId, Integer quantity);
+	public Boolean updateStockOfProd(String prodId, Integer quantity) throws ProductMSException;
 	
-	public List<ProductDTO> viewAllProducts();
+	public List<ProductDTO> viewAllProducts() throws ProductMSException;
 }
