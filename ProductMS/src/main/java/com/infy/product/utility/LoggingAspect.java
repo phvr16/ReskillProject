9d31d985941d8f282bd8cbafca44616825fbbf75
Ponugoti Harsha <harsha.ponugoti11@gmail.com>
2021-06-27 13:30:58 +0530

    Added loggingAspect,Tablescripts

---
+++ ProductMS/src/main/java/com/infy/product/utility/LoggingAspect.java
@@ -0,0 +1,29 @@
+package com.infy.product.utility;
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import com.infy.product.exception.ProductMSException;
+
+@Component
+@Aspect
+public class LoggingAspect {
+	@AfterThrowing(pointcut = "execution(* com.oms.product.service.*Impl.*(..))", throwing = "exception")	
+	public void logExceptionFromService(Exception exception) throws ProductMSException {
+			log(exception);
+	}
+
+	
+	private void log(Exception exception) {
+		Logger logger = LogManager.getLogger(this.getClass());
+		if(exception.getMessage()!=null ){
+			
+			logger.info(exception.getMessage());
+		}
+		else{
+			logger.info(exception.getMessage(), exception);
+		}
+	}
+}
